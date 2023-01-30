fc161fe054ec59421282c93b19b7dc8ac115f927
thomas-dias-esiee <103457000+thomas-dias-esiee@users.noreply.github.com>
2023-01-30 17:01:45 +0100

    Create Humain.py

---
+++ Humain.py
@@ -0,0 +1,25 @@
+#objet Robot qui puisse etre réparer marcher communiquer
+
+#classe représentant un humain : donner des ordres, se deplacer et supervier
+
+#instancier
+
+
+class Humain:
+    def marcher(self, deplacement):
+        print("Je marche")
+
+    def superviser(self, message):
+        print("Je communique:", message)
+
+    def ordres(self, ordres):
+
+
+# Instanciation  Humain
+Lucie = Humain()
+
+# Appel des fonctions marcher et communiquer
+Lucie.marcher()
+Lucie.communiquer("allo")
+
+
