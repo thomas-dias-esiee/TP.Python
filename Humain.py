83efd2c917e2a80410dcc8ff60df9122ecc96e6b
Jak'Sho <74248782+TagirAntony@users.noreply.github.com>
2023-01-30 17:08:47 +0100

    Update Humain.py

--- Humain.py
+++ Humain.py
@@ -21,15 +21,3 @@ Lucie = Humain()
 # Appel des fonctions marcher et communiquer
 Lucie.marcher()
 Lucie.communiquer("allo")
-
-# Class du robot 1
-class Robot:
-    def __init__(self, nom, marque):
-        self.nom = nom
-        self.marque = marque
-
-# On donne les valeurs
-robo1 = Robot('Robotnik', 'apple')
-robin = Robot('Robocop', 'Samsung')
-
-print()
