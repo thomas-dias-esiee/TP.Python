20cbec051d70ef201e7ee706d9046710a255baa4
Jak'Sho <74248782+TagirAntony@users.noreply.github.com>
2023-01-30 17:09:12 +0100

    Add files via upload

---
+++ robotest.py
@@ -0,0 +1,11 @@
+# Class du robot 1
+class Robot:
+    def __init__(self, nom, marque):
+        self.nom = nom
+        self.marque = marque
+
+# On donne les valeurs
+robo1 = Robot('Robotnik', 'apple')
+robin = Robot('Robocop', 'Samsung')
+
+print()
